0195affa868cd74b7d5d48f005c3c0cd83887c4a
Seth Kwame Addo <addoseth7@gmail.com>
2023-07-22 16:52:09 +0100

    added the select option to the header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,6 +7,19 @@ export default function Header() {
         <h1>
             POPULAR HORNED BEASTS IN THE WORLD
         </h1>
+        <form>
+          <fieldset>
+        
+              <label for="selectName">Horn Number
+              <select  id='selectName' name='selectName'>
+                <option value="select one" disabled selected>select one</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+              </select>
+              </label>
+          </fieldset>
+        </form>
     </header>
   )
 }
